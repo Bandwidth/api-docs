4bc0cacd357ee79a42b335b091e17a8a174b6e13
jmulford-bandwidth <jmulford@bandwidth.com>
2020-12-16 13:23:15 -0500

    removed dev branch

--- .github/actions/publish-postman/entrypoint.sh
+++ .github/actions/publish-postman/entrypoint.sh
@@ -5,8 +5,7 @@ cd publish-postman
 
 git init
 git remote add origin https://$DX_GIT_USER:$DX_GIT_TOKEN@github.com/Bandwidth/postman.git
-git pull origin dev #change dev to main
-git checkout dev #remove
+git pull origin main 
 
 cp ../$POSTMAN_FILE .
 git add .
@@ -15,6 +14,6 @@ git config user.email $DX_GIT_EMAIL
 git diff --quiet && git diff --staged --quiet || git commit -m "Collection update"
 #taken from https://stackoverflow.com/questions/22040113/how-to-let-jenkins-git-commit-only-if-there-are-changes
 #git commit will fail the build if there are no file changes
-git push origin dev #change dev to main
+git push origin main
 #git push will not fail the build if there is nothing to push
 cd ..
