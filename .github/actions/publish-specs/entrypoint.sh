276d5eb92aff80f62edc15032fe2cef93cb6409e
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2020-12-17 10:07:15 -0500

    Dx 1624 updated public resource management (#13)
    * Reverted ignore
    
    * DX-1624 changed target of postman/oas publishing
    
    * testing on dev branch
    
    * testing dev branch
    
    * fixed tokens and paths
    
    * secrets are hard
    
    * secrets are hard again, git is hard
    
    * i am bad
    
    * removed dev branch
    
    * readded checkout

--- .github/actions/publish-specs/entrypoint.sh
+++ .github/actions/publish-specs/entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-mkdir push-specs
-cd push-specs
+mkdir publish-specs
+cd publish-specs
+
 git init
-git remote add origin https://$DX_GIT_USER:$DX_GIT_TOKEN@github.com/Bandwidth/examples.git
-git pull origin master
-cd $EXAMPLES_DIRECTORY
-cp -R ../../$BANDWIDTH_SDKS_DIRECTORY/* .
+git remote add origin https://$DX_GIT_USER:$DX_GIT_TOKEN@github.com/Bandwidth/openapi-specs.git
+git pull origin main
+git checkout main
+
+cp -R ../$SPECS_DIRECTORY/* .
 git add .
 git config user.name $DX_GIT_USER
 git config user.email $DX_GIT_EMAIL
-git diff --quiet && git diff --staged --quiet || git commit -m "Spec update"
+git diff --quiet && git diff --staged --quiet || git commit -m "Specs update"
 #taken from https://stackoverflow.com/questions/22040113/how-to-let-jenkins-git-commit-only-if-there-are-changes
 #git commit will fail the build if there are no file changes
-git push origin master
+git push origin main
 #git push will not fail the build if there is nothing to push
+cd ..
