8e6e86ae1180ea6e30483bdbbfe53c186d543fb6
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2021-01-08 16:01:02 -0500

    bandwidth sdks integration (#18)
    * proof of concept for having bandwidth-sdks functionality in external-api-docs. I hope I didn't mess up something
    
    * moved iris to ignore
    
    * fixed bad syntax
    
    * temp check
    
    * matrix jank test
    
    * examples are hard to read
    
    * last test
    
    * reverted test code, added in remaining commands
    
    * DX-1704
    
    * removed out directory and re-added missing gitignore
    
    * renamed to push-sdks

---
+++ .github/actions/push-to-sdk-repo/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#unzip folder
+mkdir unzip-tmp
+cd unzip-tmp
+mv ../sdk-generation/bandwidth.zip .
+unzip bandwidth.zip
+rm bandwidth.zip
+cd ..
+
+#setup repo
+mkdir git-push
+cd git-push
+git init
+git remote add origin "https://$BW_GITHUB_USERNAME:$BW_GITHUB_PASSWORD@github.com/Bandwidth/$1.git"
+git pull origin main
+
+#generate branch
+BRANCH=$(date +"expr "%Y-%m-%d-%H-%M-%S"" | sh)
+BRANCH="release/${BRANCH}"
+git checkout -b "$BRANCH"
+
+#add files. cp -R forces overwites and preserves directory structure
+#mv does not do the same
+cp -R ../unzip-tmp/bandwidth/$3* .
+rm -rf ../unzip-tmp
+
+#checkout files to ignore
+ignoreFileString=$2 #I don't know why the parameter needs to be reassigned, but
+#shell is unhappy with doing an if statement with the variable as $2
+if test ${#ignoreFileString} -gt 0; then
+    ignoreFileList=$(echo $ignoreFileString | tr " " "\n")
+    for ignoreFile in $ignoreFileList
+    do
+        git checkout $ignoreFile
+    done
+fi
+
+git add .
+git config user.name $BW_GITHUB_USERNAME
+git config user.email $BW_GITHUB_EMAIL
+git commit -m "New deploy"
+git push --set-upstream origin "$BRANCH"
+
+#Set current directory back to the initial location
+cd ..
+rm -rf git-push
---
+++ sdk-generation/src/generate.ts
@@ -0,0 +1,164 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
+import axios from "axios";
+import FormData from "form-data";
+import fs, { readJSONSync } from "fs-extra";
+import merge from "deepmerge";
+import commandLineArgs, { OptionDefinition } from "command-line-args";
+import { LangaugeConfig, MasterConfig } from "./interfaces";
+
+const optionDefinitions: OptionDefinition[] = [
+  { name: "language", alias: "l", type: String },
+  { name: "spec", alias: "s", type: String, defaultOption: true },
+];
+
+const clOptions = commandLineArgs(optionDefinitions);
+
+const targetLang = clOptions.language;
+const spec = clOptions.spec || undefined;
+
+// Grab all the configs and the spec namespaces
+const masterConfig: MasterConfig = readJSONSync(
+  process.env.MASTER_CONFIG || "./config/masterConfig.json"
+);
+const languageConfig: LangaugeConfig = readJSONSync(
+  `${masterConfig.codeLangRoot}/${targetLang}/${masterConfig.codeLangConfig}`
+);
+
+// Grab all the APIMatic meta config files, merge them together with the language version
+const globalMetaConfig = readJSONSync(masterConfig.apimaticMetaConfig);
+const languageMetaConfig = readJSONSync(
+  `${masterConfig.codeLangRoot}/${targetLang}/${languageConfig.apimaticMetaConfig}`
+);
+const metaConfig = merge(globalMetaConfig, languageMetaConfig);
+
+// list of dir in ./specs, [ 'Voice', 'Messaging', 'WebRtc', ...]
+const specs = fs.readdirSync(masterConfig.apiSpecRoot, "utf8");
+
+// Make sure the commandline arg is one of the directories
+if (spec && !specs.includes(spec)) {
+  throw new Error(`Spec not found ${spec}, available specs ${specs.join(",")}`);
+}
+
+async function main() {
+  const formData = new FormData();
+
+  // Add all the specs to the formdata
+  addOpenApiSpecs(formData);
+
+  // add the merged APIMatic metaConfig
+  formData.append("global", JSON.stringify(metaConfig), {
+    contentType: "application/octet-stream",
+    filename: "global.json",
+  });
+
+  // Added the language package information
+  if (languageConfig.packageInformation) {
+    formData.append(
+      "packageInformation",
+      JSON.stringify(
+        readJSONSync(
+          `${masterConfig.codeLangRoot}/${targetLang}/${languageConfig.packageInformation}`
+        )
+      ),
+      {
+        contentType: "application/octet-stream",
+        filename: "packageInformation.json",
+      }
+    );
+  }
+
+  try {
+    const response = await axios.post(
+      `${masterConfig.generateURL}=${languageConfig.apimaticTemplateId}`,
+      formData,
+      {
+        auth: {
+          username: process.env.APIMATIC_USER || "",
+          password: process.env.APIMATIC_PASS || "",
+        },
+        headers: {
+          "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
+        },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const stream = response.data;
+
+    // Write the zip file
+    fs.writeFileSync(`./bandwidth.zip`, stream);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+// Wraps all the props in an 'allOf' for XML
+function wrapRef(oas: any) {
+  const { schemas } = oas.components;
+
+  Object.keys(schemas).forEach((schemaName) => {
+    const props = schemas[schemaName].properties;
+
+    if (!props) return;
+
+    Object.keys(props).forEach((propName) => {
+      const item = props[propName];
+
+      if (item.$ref && item.xml) {
+        item.allOf = [
+          {
+            $ref: item.$ref,
+          },
+        ];
+        delete item.$ref;
+      }
+    });
+  });
+
+  return oas;
+}
+
+function addOpenApiSpecs(formData: FormData) {
+  if (spec === undefined) {
+    for (const apiNamespace of specs) {
+      const apiSpecPath = `${masterConfig.apiSpecRoot}/${apiNamespace}/${masterConfig.apiSpecFile}`;
+
+      if (targetLang !== "phpold" || apiNamespace !== "Iris") {
+        let stream;
+        if (masterConfig.wrappedRef.includes(apiNamespace)) {
+          const apiSpec = readJSONSync(apiSpecPath);
+          const wrappedSpec = wrapRef(apiSpec);
+          stream = JSON.stringify(wrappedSpec);
+        } else {
+          stream = JSON.stringify(readJSONSync(apiSpecPath));
+        }
+
+        formData.append("apiDescriptions", stream, {
+          contentType: "application/octet-stream",
+          filename: "api.json",
+        });
+      }
+    }
+  } else {
+    // Add only the one spec to the form data
+    const apiSpecPath = `${masterConfig.apiSpecRoot}/${spec}/${masterConfig.apiSpecFile}`;
+
+    let stream;
+    if (masterConfig.wrappedRef.includes(spec)) {
+      const apiSpec = readJSONSync(apiSpecPath);
+      const wrappedSpec = wrapRef(apiSpec);
+      stream = JSON.stringify(wrappedSpec);
+    } else {
+      stream = JSON.stringify(readJSONSync(apiSpecPath));
+    }
+
+    formData.append("apiDescriptions", stream, {
+      contentType: "application/octet-stream",
+      filename: `api.json`,
+    });
+  }
+}
+
+// run the main functions
+main();
