1367cdd20c8bb0250ce1eb5715b6f1e6be5ec8c3
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2021-02-09 15:08:25 -0500

    DX-1790 made generate script revert file changes (#39)

--- docsite-generation/generate.sh
+++ docsite-generation/generate.sh
@@ -2,17 +2,27 @@
 
 cd ..
 
+#Setup package for generation
+rm -rf package/
 mkdir package
-mv configuration/Global specs/
-mv specs package/
-mv package/specs package/spec
-mv content package/
-mv static package/
-mv bw-portal.APIMATIC-BUILD.json package/bw-portal.APIMATIC-BUILD.json
+mkdir package/spec
+cp -R specs/* package/spec/
+cp -R configuration/Global package/spec
+cp -R content package/
+cp -R static package/
+cp bw-portal.APIMATIC-BUILD.json package/bw-portal.APIMATIC-BUILD.json
 zip -r package.zip package
+
+#Generate package
 curl titan.apimatic.io/api/build -F 'file=@package.zip' -o portal.zip
 mkdir portal
 unzip -o portal.zip -d portal
+
+#Cleanup unneeded files
+rm -rf package
+rm package.zip
+rm portal.zip
+
 if [ ! -f portal/index.html ]; then
     echo "Docsite failed to generate"
     exit 1
