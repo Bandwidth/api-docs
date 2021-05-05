bc00bd94a0d87d468e8607cdb6bdfe013fb916c3
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2021-05-05 09:40:41 -0400

    Dx 1984 (#106)
    * DX-1984 python script to add code snippets to our OAS
    
    * Updated package.json script and converted python script to JS
    
    * Made specs-source directory
    
    * made custom code command
    
    * made custom code more gooder

---
+++ site/codeSnippets/Voice/getCall/sample.rb
@@ -0,0 +1,3 @@
+puts 'hello world'
+
+puts 'hello again'
---
+++ site/scripts/add-code-snippets-oas.js
@@ -0,0 +1,78 @@
+/*
+ * add-code-snippets-to-oas.js
+ *
+ * This script runs on docsite generation to modify the OpenAPI specs to support Redoc's code snippet extension.
+ *
+ * The code sample files are stored in the following format
+ * 
+ *     codeSnippets/<SpecName>/<OperationId>/sample.<extension>
+ * 
+ * Pseudo code for the script
+ * 
+ * for each OpenAPI spec:
+ *     read into a JSON object
+ *     for each operationId:
+ *         add the x-code-samples field and default it to []
+ *         build the file path
+ *         for each file:
+ *             add the object {
+ *                 "lang": "<switch on lang based on file extension>",
+ *                 "source": "<file contents>"
+ *             }
+ *     save the OpenAPI spec
+ */
+const fs = require('fs');
+
+//Converts the file extension to the proper Redoc `lang` tag
+const EXTENSION_TO_LANG = {
+    "cs": "C#",
+    "java": "Java",
+    "php": "PHP",
+    "py": "Python",
+    "rb": "Ruby",
+    "ts": "TypeScript",
+    "js": "JavaScript"
+};
+
+var spec_name = '';
+var operation_id = '';
+const SPEC_SOURCE_DIRECTORY = 'specs-source/';
+const SPEC_OUTPUT_DIRECTORY = 'specs/';
+
+var files = fs.readdirSync(SPEC_SOURCE_DIRECTORY);
+files.forEach(spec => {
+    var spec_json = JSON.parse(fs.readFileSync(SPEC_SOURCE_DIRECTORY + spec));
+    spec_name = spec_json["info"]["title"];
+
+    //This results in every URL defined in the OpenAPI spec
+    for (var path in spec_json["paths"]) {
+        var path_object = spec_json["paths"][path];
+
+        //This results in every HTTP method defined on the URL
+        for (var operation in path_object) {
+            var operation_object = path_object[operation];
+
+            //operation_id is what we are matching code samples on
+            operation_id = operation_object["operationId"];
+            var sample_files_directory = `codeSnippets/${spec_name}/${operation_id}/`;
+
+            //Add the x-code-samples field, default to an empty list
+
+            //If code samples exist, add them
+            if (fs.existsSync(sample_files_directory)) {
+                operation_object["x-code-samples"] = [];
+                var sample_code_files = fs.readdirSync(sample_files_directory);
+                 sample_code_files.forEach(sample_code_file => {
+                    var redoc_lang = EXTENSION_TO_LANG[sample_code_file.split(".").slice(-1)];
+                    var sample_code_file_contents = fs.readFileSync(sample_files_directory + sample_code_file, 'utf8');
+
+                    operation_object["x-code-samples"].push({
+                        "lang": redoc_lang,
+                        "source": sample_code_file_contents
+                    });
+                });
+            }
+        }
+    }
+    fs.writeFileSync(SPEC_OUTPUT_DIRECTORY + spec, JSON.stringify(spec_json, null, 4), { flag: 'w+' });
+});
