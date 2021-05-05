dad41068e5fbc3c3bc79ed99cb9a11ae75657912
Mike Hamer <mhamer@bandwidth.com>
2021-05-05 13:34:49 -0400

    Quick sample for create session

---
+++ site/code-snippets/WebRtc/createSession/sample.cs
@@ -0,0 +1,14 @@
+using Bandwidth.Standard;
+using Bandwidth.Standard.WebRtc.Models;
+
+var client = new BandwidthClient.Builder()
+    .Environment(Bandwidth.Standard.Environment.Production)
+    .WebRtcBasicAuthCredentials("username", "password")
+    .Build();
+
+var session = new Session()
+{
+    Tag = "new-session"
+};
+
+var response = await _client.WebRtc.APIController.CreateSessionAsync("1111111", session);
