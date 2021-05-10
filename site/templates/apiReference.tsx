3a75c397b9363ec5df9961c9eee73836d02a03c8
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-05-10 11:25:58 -0400

    Update apiReference.tsx

--- site/templates/apiReference.tsx
+++ site/templates/apiReference.tsx
@@ -1,3 +1,27 @@
+// TODO: Details
+// So for all of those commented out lines in the template file -
+// ReDoc has an issue with being passed a variable for certain color fields, it was even noted in that guide we followed to get started
+//
+//    Unfortunately if you try to use the custom property names like that with most of what’s under colors, you’ll hit an error,
+//    since Redoc does some processing to generate light/dark variants etc and expects the values you give it to be parseable as HEX or RGB.
+//    I think this could be addressed though.
+//
+// So i put those placeholders in there because its what we need to figure out to get dark mode integrated
+// for the redoc component without using that 3rd party theme
+//
+// typography: {
+//             fontSize: 'var(--ifm-font-size-base)'
+//
+// resolves just fine for some reason with var(…) resolving to a hex value
+// but all of that color stuff explodes and redoc throws an error saying youve passed in an invalid value.
+//
+// Passing in a hex or rgb value works just fine (which oddly enough thats what the var(…) is supposed to do
+// But all of those settings are poorly documented so i wanted to get a template going with all of the available
+// config stuff so that we know what we can control when the time comes.
+
+// I opened up an issue on redocs repo https://github.com/Redocly/redoc/issues/1607
+// asking them to update their readme because their options are out of date
+
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
