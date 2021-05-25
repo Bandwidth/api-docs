6566337882fdbf7cc3354c9fb17c16e18beee02e
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-05-25 10:57:15 -0400

    css is dumb

--- site/src/pages/numbers-api-reference.tsx
+++ site/src/pages/numbers-api-reference.tsx
@@ -8,30 +8,31 @@ export default function ApiReference() {
     return (
         <Layout>
             <main>
-            <br/>
-            <div class="a">
-              Emojis are explicitly not allowed on any endpoint for this API.
-            </div><br/>
-            <div class="a">
-              This API expects phone numbers to be in a 10 digit format <code>XXXYYYZZZZ</code>, not in E164 format.
-            </div>
-            <div class="a">
-              This is different from the voice and messaging APIs, which expect phone numbers in E164 format.
-            </div><br/>
-            <div class="a">
-              Base API URL:
-            </div>
-            <div class="a">
-              <code>https://dashboard.bandwidth.com/api/</code>
-            </div>
-            <Iframe url="https://dev.bandwidth.com/numbers/apiRef.html"
-                    width="900px"
-                    height="650px"
-                    id="irisRaml"
-                    className="irisRaml"
-                    display="inline"
-                    position="relative"
-                    frameBorder="0"/>
+              <br/>
+              <div class="a">
+                Emojis are explicitly not allowed on any endpoint for this API.
+              </div><br/>
+              <div class="a">
+                This API expects phone numbers to be in a 10 digit format <code>XXXYYYZZZZ</code>, not in E164 format.
+              </div>
+              <div class="a">
+                This is different from the voice and messaging APIs, which expect phone numbers in E164 format.
+              </div><br/>
+              <div class="a">
+                Base API URL:
+              </div>
+              <div class="a">
+                <code>https://dashboard.bandwidth.com/api/</code>
+              </div>
+              <div class="a">
+                <Iframe url="https://dev.bandwidth.com/numbers/apiRef.html"
+                        width="900px"
+                        height="650px"
+                        id="irisRaml"
+                        display="inline"
+                        position="relative"
+                        frameBorder="0"/>
+              </div>
             </main>
         </Layout>
     );
