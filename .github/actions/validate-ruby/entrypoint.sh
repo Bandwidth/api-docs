2816bf7e50f6b211e5f7a14b630640bbbbf8f658
jmulford-bandwidth <jmulford@bandwidth.com>
2021-05-27 17:05:05 -0400

    readded get messages and added skip file option

--- .github/actions/validate-ruby/entrypoint.sh
+++ .github/actions/validate-ruby/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gem install bandwidth-sdk
-python3 site/scripts/validate_code_snippets.py rb ruby
+python3 site/scripts/validate_code_snippets.py rb ruby "site/code-snippets/Messaging/GetMessages/sample.rb"
