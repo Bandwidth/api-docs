4ecd4d3bb63dfb151bba7b559122ac5cf6782a18
jmulford-bandwidth <jmulford@bandwidth.com>
2021-05-27 11:40:26 -0400

    fixed ruby sample

--- site/code-snippets/Messaging/GetMessages/sample.rb
+++ site/code-snippets/Messaging/GetMessages/sample.rb
@@ -10,7 +10,7 @@ bandwidth_client = Bandwidth::Client.new(
 messaging_client = bandwidth_client.messaging_client.client
 
 begin
-  result = messaging_client.get_messages(ENV['BW_ACCOUNT_ID'], source_tn: ENV['BW_NUMBER'])
+  result = messaging_client.get_messages(ENV['BW_ACCOUNT_ID'], :source_tn => ENV['BW_NUMBER'])
   puts result.data.total_count
 rescue APIException => e
   puts e.response_code
