4b293b7c30289a029f789e1dd4c78c54a717f23d
jmulford-bandwidth <jmulford@bandwidth.com>
2021-05-28 15:28:06 -0400

    added remaining ruby samples

---
+++ site/code-snippets/Voice/getConference/sample.rb
@@ -0,0 +1,20 @@
+require 'bandwidth'
+  
+include Bandwidth
+include Bandwidth::Voice
+
+bandwidth_client = Bandwidth::Client.new(
+    voice_basic_auth_user_name: ENV['BW_USERNAME'],
+    voice_basic_auth_password: ENV['BW_PASSWORD']
+)
+
+voice_client = bandwidth_client.voice_client.client
+
+conference_id = "conf-1234"
+
+begin
+    response = voice_client.get_conference_by_id(ENV['BW_ACCOUNT_ID'], conference_id)
+    print(response.data.name)
+rescue APIException => e
+    puts e.response_code
+end
