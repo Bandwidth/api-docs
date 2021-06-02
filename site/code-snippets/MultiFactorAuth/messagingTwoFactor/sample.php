fa00f9a1ec9abcd650fe192b1821c23a15f11d8f
jmulford-bandwidth <jmulford@bandwidth.com>
2021-06-02 10:20:46 -0400

    Added php mfa

---
+++ site/code-snippets/MultiFactorAuth/messagingTwoFactor/sample.php
@@ -0,0 +1,34 @@
+<?php
+
+require "vendor/autoload.php";
+
+$BW_USERNAME = getenv("BW_USERNAME");
+$BW_PASSWORD = getenv("BW_PASSWORD");
+$BW_ACCOUNT_ID = getenv("BW_ACCOUNT_ID");
+$BW_MFA_MESSAGING_APPLICATION_ID = getenv("BW_MFA_MESSAGING_APPLICATION_ID");
+$BW_MFA_NUMBER = getenv("BW_MFA_NUMBER");
+$USER_NUMBER = getenv("USER_NUMBER");
+
+$config = new BandwidthLib\Configuration(
+    array(
+        'twoFactorAuthBasicAuthUserName' => $BW_USERNAME,
+        'twoFactorAuthBasicAuthPassword' => $BW_PASSWORD,
+    )
+);
+$client = new BandwidthLib\BandwidthClient($config);
+
+$mfaClient = $client->getTwoFactorAuth()->getMFA();
+
+$body = new BandwidthLib\TwoFactorAuth\Models\TwoFactorCodeRequestSchema();
+$body->from = $BW_MFA_NUMBER;
+$body->to = $USER_NUMBER;
+$body->applicationId = $BW_MFA_MESSAGING_APPLICATION_ID;
+$body->scope = "scope";
+$body->digits = 6;
+$body->message = "Your temporary {NAME} {SCOPE} code is {CODE}";
+
+try {
+    $mfaClient->createMessagingTwoFactor($BW_ACCOUNT_ID, $body);
+} catch (BandwidthLib\APIException $e) {
+    print_r($e->getResponseCode());
+}
