9b93dacefad573d00ff47ff4e0c7f8aa8698f734
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-06-03 16:38:09 -0400

    1st round of css changes

---
+++ site/src/css/redocTheme.ts
@@ -0,0 +1,114 @@
+const DOCUSAURUS = {
+  darkGray: '#303846',
+  dark: {
+    primaryText: '#FFFFFF',
+    secondaryText: 'rgba(255, 255, 255, 1)',
+    backgroundColor: '#121212',
+  },
+};
+
+/**
+ * NOTE: Variables taken from infima
+ * @see https://github.com/facebookincubator/infima/blob/master/packages/core/styles/common/variables.pcss
+ */
+export function lightTheme(primaryColor) {
+    return({
+      typography: {
+        fontFamily: 'var(--ifm-font-family-base)',
+        fontSize: 'var(--ifm-font-size-base)',
+        lineHeight: 'var(--ifm-line-height-base)',
+        fontWeightLight: 'var(--ifm-font-weight-light)',
+        fontWeightRegular: 'var(--ifm-font-weight-base)',
+        fontWeightBold: 'var(--ifm-font-weight-bold)',
+        headings: {
+          fontFamily: 'var(--ifm-font-family-base)',
+          fontWeight: 'var(--ifm-font-weight-semibold)',
+          lineHeight: 'var(--ifm-line-height-base)',
+        },
+        code: {
+          fontFamily: 'var(--ifm-font-family-monospace)',
+          lineHeight: 'var(--ifm-pre-line-height)',
+        },
+      },
+      colors: {
+        primary: {
+            main: primaryColor
+        },
+        gray: {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+        },
+        border: {
+          dark: '#ffffff',
+          light: 'rgba(0,0,0, 0.1)',
+        },
+      },
+      sidebar: {
+        width: '300px',
+        backgroundColor: '#ffffff',
+      },
+      rightPanel: {
+        backgroundColor: 'transparent',
+      }
+    }
+  )
+};
+
+export function darkTheme(primaryColor) {
+  return({
+      typography: {
+        fontFamily: 'var(--ifm-font-family-base)',
+        fontSize: 'var(--ifm-font-size-base)',
+        lineHeight: 'var(--ifm-line-height-base)',
+        fontWeightLight: 'var(--ifm-font-weight-light)',
+        fontWeightRegular: 'var(--ifm-font-weight-base)',
+        fontWeightBold: 'var(--ifm-font-weight-bold)',
+        headings: {
+          fontFamily: 'var(--ifm-font-family-base)',
+          fontWeight: 'var(--ifm-font-weight-semibold)',
+          lineHeight: 'var(--ifm-line-height-base)',
+        },
+        code: {
+          fontFamily: 'var(--ifm-font-family-monospace)',
+          lineHeight: 'var(--ifm-pre-line-height)',
+        },
+        links: {
+          color: '#ffffff'
+        }
+      },
+      colors: {
+        primary: {
+            main: primaryColor
+        },
+        text: {
+          primary: DOCUSAURUS.dark.primaryText,
+          secondary: DOCUSAURUS.dark.secondaryText,
+        },
+        gray: {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+        },
+        border: {
+          dark: '#ffffff',
+          light: 'rgba(0,0,0, 0.1)',
+        },
+      },
+      schema: {
+        nestedBackground: DOCUSAURUS.dark.backgroundColor,
+        typeNameColor: DOCUSAURUS.dark.secondaryText,
+        typeTitleColor: DOCUSAURUS.dark.secondaryText,
+      },
+      sidebar: {
+        width: '300px',
+        backgroundColor: DOCUSAURUS.dark.backgroundColor,
+        textColor: DOCUSAURUS.dark.primaryText,
+        arrow: {
+          color: DOCUSAURUS.dark.primaryText,
+        },
+      },
+      rightPanel: {
+        backgroundColor: DOCUSAURUS.dark.backgroundColor,
+      },
+    }
+  )
+};
