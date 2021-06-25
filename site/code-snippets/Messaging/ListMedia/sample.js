909dcd0862e26eee7590b3d1d8264ac794f0a227
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-06-25 14:16:56 -0400

    Add listMedia sample

---
+++ site/code-snippets/Messaging/ListMedia/sample.js
@@ -0,0 +1,24 @@
+import { Client, ApiController } from '@bandwidth/messaging';
+import * as fs from 'fs';
+
+const BW_USERNAME = process.env["BW_USERNAME"];
+const BW_PASSWORD = process.env["BW_PASSWORD"];
+const BW_ACCOUNT_ID = process.env["BW_ACCOUNT_ID"];
+const mediaId = "abc123.pdf";
+
+const client = new Client({
+  basicAuthUserName: BW_USERNAME,
+  basicAuthPassword: BW_PASSWORD
+});
+
+const controller = new ApiController(client);
+
+const listMedia = async function() {
+  try {
+    const response = await controller.listMedia(BW_ACCOUNT_ID);
+    console.log(response.result)
+  } catch (error) {
+  console.error(error);
+}};
+
+listMedia();
