7e0e3eb60ff5017920ddc47b1a85474239518d71
Mike Hamer <mhamer@bandwidth.com>
2021-06-28 09:27:08 -0400

    Remove unused variables

--- site/code-snippets/Voice/getCall/sample.cs
+++ site/code-snippets/Voice/getCall/sample.cs
@@ -1,11 +1,6 @@
 var username = System.Environment.GetEnvironmentVariable("BW_USERNAME");
 var password = System.Environment.GetEnvironmentVariable("BW_PASSWORD");
 var accountId = System.Environment.GetEnvironmentVariable("BW_ACCOUNT_ID");
-var applicationId = System.Environment.GetEnvironmentVariable("BW_VOICE_APPLICATION_ID");
-var to = System.Environment.GetEnvironmentVariable("USER_NUMBER");
-var from = System.Environment.GetEnvironmentVariable("BW_NUMBER");
-var baseUrl = System.Environment.GetEnvironmentVariable("BASE_CALLBACK_URL");
-var answerUrl = string.Concat(baseUrl, "/callbacks/answer");
 
 // Call id is returned after successfully creating a call.
 var callId = "c-95ac8d6e-1a31c52e-b38f-4198-93c1-51633ec68f8d";
