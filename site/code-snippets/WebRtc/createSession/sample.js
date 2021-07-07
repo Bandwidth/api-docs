c7abfcc8d71ddedbabd13d1fccaa7086f5553faf
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-07 16:56:40 -0400

    Add WebRTC Samples

---
+++ site/code-snippets/WebRtc/createSession/sample.js
@@ -0,0 +1,27 @@
+import { Client, ApiController } from '@bandwidth/webrtc';
+
+const BW_USERNAME = process.env["BW_USERNAME"];
+const BW_PASSWORD = process.env["BW_PASSWORD"];
+const BW_ACCOUNT_ID = process.env["BW_ACCOUNT_ID"];
+
+const client = new Client({
+  basicAuthUserName: BW_USERNAME,
+  basicAuthPassword: BW_PASSWORD
+});
+
+const controller = new ApiController(client);
+
+const accountId = BW_ACCOUNT_ID;
+
+const createSession = async function() {
+  try{
+    const response = await controller.createSession(accountId, {
+      tag: '{"SessionName": "my_session"}'
+    })
+    console.log(response.body);
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+createSession();
---
+++ site/code-snippets/WebRtc/listSessionParticipants/sample.js
@@ -0,0 +1,26 @@
+import { Client, ApiController } from '@bandwidth/webrtc';
+
+const BW_USERNAME = process.env["BW_USERNAME"];
+const BW_PASSWORD = process.env["BW_PASSWORD"];
+const BW_ACCOUNT_ID = process.env["BW_ACCOUNT_ID"];
+
+const client = new Client({
+  basicAuthUserName: BW_USERNAME,
+  basicAuthPassword: BW_PASSWORD
+});
+
+const controller = new ApiController(client);
+
+const accountId = BW_ACCOUNT_ID;
+const sessionId = "75c21163-e110-41bc-bd76-1bb428ec85d5"
+
+const listSessionParticipants = async function() {
+  try{
+    const response = await controller.listSessionParticipants(accountId, sessionId)
+    console.log(response.body);
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+listSessionParticipants();
