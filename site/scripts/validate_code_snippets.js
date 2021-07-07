2df9f24922fab255deb3d681c8fc29262aab93e2
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-07 11:00:13 -0400

    Update validate_code_snippets.js

--- site/scripts/validate_code_snippets.js
+++ site/scripts/validate_code_snippets.js
@@ -15,15 +15,52 @@ that are order dependent will be handled in the SDK repos.
 //   console.error(`stderr: ${stderr}`);
 // });
 
+const { exec } = require("child_process");
 const fs = require("fs")
+var path = require('path')
 
 try {
   const arrayOfFiles = fs.readdirSync("../code-snippets")
-  arrayOfFiles.forEach (element => {
-    const snippetFiles = fs.readdirSync("../code-snippets/${element}")
-    console.log(snippetFiles)
+  arrayOfFiles.forEach (product => {
+    const operationFiles = fs.readdirSync("../code-snippets/" + product)
+    operationFiles.forEach (operation => {
+      const sampleFileArray = fs.readdirSync("../code-snippets/" + product + "/" + operation)
+      // console.log(sampleFileArray)
+      sampleFileArray.forEach(file => {
+        if (path.extname(file) == ".js"){
+          // console.log(file)
+          exec(`node ../code-snippets/${product}/${operation}/${file}`, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+          });
+        }
+      })
+    })
   });
-  console.log(arrayOfFiles)
 } catch(e) {
   console.log(e)
 }
+
+// const glob = require("glob");
+//
+// var getDirectories = function (src, callback) {
+//   glob(src + '/**/*', callback);
+// };
+//
+// const fileArray = getDirectories('../code-snippets', function (err, res) {
+//   if (err) {
+//     console.log('Error', err);
+//   }
+// });
+//
+// try {
+//   fileArray.forEach (file => {
+//     console.log(file)
+//   })
+// } catch(e) {
+//     console.log(e)
+// }
