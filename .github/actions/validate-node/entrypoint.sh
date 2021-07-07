348719127f2e8716f73f5708f6e0220ed9aff072
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-07 13:54:15 -0400

    Skip Node_modules files

--- .github/actions/validate-node/entrypoint.sh
+++ .github/actions/validate-node/entrypoint.sh
@@ -2,4 +2,4 @@
 
 npm install
 cd site/code-snippets && npm install && cd .. && cd ..
-python3 site/scripts/validate_code_snippets.py js node "site/code-snippets/node_modules/*"
+python3 site/scripts/validate_code_snippets.py js node
