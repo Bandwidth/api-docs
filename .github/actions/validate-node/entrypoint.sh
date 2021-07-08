cbda19262ddab7fc3785d4d64b966499e443e4aa
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-08 13:42:59 -0400

    Update entrypoint.sh
    https://nodejs.dev/learn/update-all-the-nodejs-dependencies-to-their-latest-version

--- .github/actions/validate-node/entrypoint.sh
+++ .github/actions/validate-node/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 npm install
-cd site/code-snippets && npm install && npm update && cd .. && cd ..
+cd site/code-snippets
+npm install -g npm-check-updates
+ncu -u && npm update
+npm install && cd .. && cd ..
 python3 site/scripts/validate_code_snippets.py js node
