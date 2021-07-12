9eb7991c2a1adf8c722c442fb68acb576fc11994
jmulford-bandwidth <jmulford@bandwidth.com>
2021-07-12 14:55:40 -0400

    changed env grabbing for ruby

--- site/code-snippets/PhoneNumberLookup/LookupRequest/sample.rb
+++ site/code-snippets/PhoneNumberLookup/LookupRequest/sample.rb
@@ -3,9 +3,13 @@ require 'bandwidth'
 include Bandwidth
 include Bandwidth::Voice
 
+BW_USERNAME = ENV['BW_USERNAME']
+BW_PASSWORD = ENV['BW_PASSWORD']
+BW_ACCOUNT_ID = ENV['BW_ACCOUNT_ID']
+
 bandwidth_client = Bandwidth::Client.new(
-    phone_number_lookup_basic_auth_user_name: ENV['BW_USERNAME'],
-    phone_number_lookup_basic_auth_password: ENV['BW_PASSWORD']
+    phone_number_lookup_basic_auth_user_name: BW_USERNAME,
+    phone_number_lookup_basic_auth_password: BW_PASSWORD
 )
 
 phone_number_lookup_client = bandwidth_client.phone_number_lookup_client.client
@@ -14,7 +18,7 @@ body = OrderRequest.new
 body.tns = ["+15554443333"]
 
 begin
-    result = phone_number_lookup_client.create_lookup_request(ENV['BW_ACCOUNT_ID'], body)
+    result = phone_number_lookup_client.create_lookup_request(BW_ACCOUNT_ID, body)
     puts result.data.request_id
 rescue APIException => e
     puts e.response_code
--- site/code-snippets/PhoneNumberLookup/LookupRequestStatus/sample.rb
+++ site/code-snippets/PhoneNumberLookup/LookupRequestStatus/sample.rb
@@ -3,9 +3,13 @@ require 'bandwidth'
 include Bandwidth
 include Bandwidth::Voice
 
+BW_USERNAME = ENV['BW_USERNAME']
+BW_PASSWORD = ENV['BW_PASSWORD']
+BW_ACCOUNT_ID = ENV['BW_ACCOUNT_ID']
+
 bandwidth_client = Bandwidth::Client.new(
-    phone_number_lookup_basic_auth_user_name: ENV['BW_USERNAME'],
-    phone_number_lookup_basic_auth_password: ENV['BW_PASSWORD']
+    phone_number_lookup_basic_auth_user_name: BW_USERNAME,
+    phone_number_lookup_basic_auth_password: BW_PASSWORD
 )
 
 phone_number_lookup_client = bandwidth_client.phone_number_lookup_client.client
@@ -13,7 +17,7 @@ phone_number_lookup_client = bandwidth_client.phone_number_lookup_client.client
 request_id = "1234-abcd"
 
 begin
-    result = phone_number_lookup_client.get_lookup_request_status(ENV['BW_ACCOUNT_ID'], request_id)
+    result = phone_number_lookup_client.get_lookup_request_status(BW_ACCOUNT_ID, request_id)
     puts result.data.status
 rescue APIException => e
     puts e.response_code
