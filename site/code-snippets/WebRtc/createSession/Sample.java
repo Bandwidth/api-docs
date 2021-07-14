23f27dfdd56a399df8f86876673497656df41626
Mike Hamer <mhamer@bandwidth.com>
2021-07-14 13:34:41 -0400

    Update constants and file case

--- site/code-snippets/WebRtc/createSession/Sample.java
+++ site/code-snippets/WebRtc/createSession/Sample.java
@@ -6,21 +6,21 @@ import com.bandwidth.webrtc.controllers.*;
 import com.bandwidth.exceptions.ApiException;
 import com.bandwidth.http.response.ApiResponse;
 
-class Sample {
-    public Sample() {
-        String username = System.getenv("BW_USERNAME");
-        String password = System.getenv("BW_PASSWORD");
-        String accountId = System.getenv("BW_ACCOUNT_ID");
+public class Sample {
+    public static final String USERNAME = System.getenv("BW_USERNAME");
+    public static final String PASSWORD = System.getenv("BW_PASSWORD");
+    public static final String ACCOUNT_ID = System.getenv("BW_ACCOUNT_ID");
 
+    public static void main(String[] args) {
         BandwidthClient client = new BandwidthClient.Builder()
-            .webRtcBasicAuthCredentials(username, password)
+            .webRtcBasicAuthCredentials(USERNAME, PASSWORD)
             .build();
 
         Session session = new Session();
         session.setTag("new-session");
 
         try {
-            ApiResponse<Session> response = client.getWebRtcClient().getAPIController().createSession(accountId, session);
+            ApiResponse<Session> response = client.getWebRtcClient().getAPIController().createSession(ACCOUNT_ID, session);
         } catch (ApiException|IOException ex) {
             // Handle exceptions from the request.
         }
