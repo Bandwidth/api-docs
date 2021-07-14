f848376d609213106526240849d9c5b59c1ef8f8
Mike Hamer <mhamer@bandwidth.com>
2021-07-14 16:33:01 -0400

    List session participants

---
+++ site/code-snippets/WebRtc/listSessionParticipants/Sample.java
@@ -0,0 +1,27 @@
+import com.bandwidth.BandwidthClient;
+import com.bandwidth.exceptions.ApiException;
+import com.bandwidth.http.response.ApiResponse;
+import com.bandwidth.webrtc.models.Participant;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Sample {
+    public static final String USERNAME = System.getenv("BW_USERNAME");
+    public static final String PASSWORD = System.getenv("BW_PASSWORD");
+    public static final String ACCOUNT_ID = System.getenv("BW_ACCOUNT_ID");
+
+    public static void main(String[] args) {
+        String sessionId = "75c21163-e110-41bc-bd76-1bb428ec85d5";
+
+        BandwidthClient client = new BandwidthClient.Builder()
+                .webRtcBasicAuthCredentials(USERNAME, PASSWORD)
+                .build();
+
+        try {
+            ApiResponse<List<Participant>> response = client.getWebRtcClient().getAPIController().listSessionParticipants(ACCOUNT_ID, sessionId);
+        } catch (ApiException|IOException ex) {
+            // Handle exceptions from the request.
+        }
+    }
+}
