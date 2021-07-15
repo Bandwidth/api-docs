3b21665bf070833d4e6088d6255e94ac88f60152
Mike Hamer <mhamer@bandwidth.com>
2021-07-15 16:02:10 -0400

    Get call transcription

---
+++ site/code-snippets/Voice/getCallTranscription/Sample.java
@@ -0,0 +1,31 @@
+import com.bandwidth.BandwidthClient;
+import com.bandwidth.exceptions.ApiException;
+import com.bandwidth.http.response.ApiResponse;
+import com.bandwidth.voice.models.RecordingMetadataResponse;
+import com.bandwidth.voice.models.TranscriptionResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Sample {
+    public static final String USERNAME = System.getenv("BW_USERNAME");
+    public static final String PASSWORD = System.getenv("BW_PASSWORD");
+    public static final String ACCOUNT_ID = System.getenv("BW_ACCOUNT_ID");
+
+    public static void main(String[] args) {
+        // Call id is returned after successfully creating a call.
+        var callId = "c-95ac8d6e-1a31c52e-b38f-4198-93c1-51633ec68f8d";
+        // Recording id is returned after retrieving a recording from the call.
+        var recordingId = "r-d68201ef-d53e-4c6d-a743-1c1283909d41";
+
+        BandwidthClient client = new BandwidthClient.Builder()
+                .webRtcBasicAuthCredentials(USERNAME, PASSWORD)
+                .build();
+
+        try {
+            ApiResponse<TranscriptionResponse> response = client.getVoiceClient().getAPIController().getRecordingTranscription(ACCOUNT_ID, callId, recordingId);
+        } catch (ApiException|IOException ex) {
+            // Handle exceptions from the request.
+        }
+    }
+}
