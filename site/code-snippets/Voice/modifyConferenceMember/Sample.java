cf01cd32f002fa128bcdc1f8b7955c962b86e8cd
Mike Hamer <mhamer@bandwidth.com>
2021-07-15 16:26:29 -0400

    Add remaining voice snippets

--- site/code-snippets/Voice/modifyConferenceMember/Sample.java
+++ site/code-snippets/Voice/modifyConferenceMember/Sample.java
@@ -1,7 +1,7 @@
 import com.bandwidth.BandwidthClient;
 import com.bandwidth.exceptions.ApiException;
 import com.bandwidth.http.response.ApiResponse;
-import com.bandwidth.voice.models.RecordingMetadataResponse;
+import com.bandwidth.voice.models.*;
 
 import java.io.IOException;
 
@@ -11,17 +11,18 @@ public class Sample {
     public static final String ACCOUNT_ID = System.getenv("BW_ACCOUNT_ID");
 
     public static void main(String[] args) {
-        // Call id is returned after successfully creating a call.
+        String conferenceId = "conf-95ac8d8d-28e06798-2afe-434c-b0f4-666a79cd47f8";
         String callId = "c-95ac8d6e-1a31c52e-b38f-4198-93c1-51633ec68f8d";
-        // Recording id is returned after retrieving a recording from the call.
-        String recordingId = "r-d68201ef-d53e-4c6d-a743-1c1283909d41";
 
         BandwidthClient client = new BandwidthClient.Builder()
                 .webRtcBasicAuthCredentials(USERNAME, PASSWORD)
                 .build();
 
+        ConferenceMemberDetail conferenceMemberDetail = new ConferenceMemberDetail();
+        conferenceMemberDetail.setMute(true);
+
         try {
-            ApiResponse<RecordingMetadataResponse> response = client.getVoiceClient().getAPIController().getMetadataForRecording(ACCOUNT_ID, callId, recordingId);
+            ApiResponse<Void> response = client.getVoiceClient().getAPIController().modifyConferenceMember(ACCOUNT_ID, conferenceId, callId, conferenceMemberDetail);
         } catch (ApiException|IOException ex) {
             // Handle exceptions from the request.
         }
--- site/code-snippets/Voice/transcribeCallRecording/Sample.java
+++ site/code-snippets/Voice/transcribeCallRecording/Sample.java
@@ -1,7 +1,7 @@
 import com.bandwidth.BandwidthClient;
 import com.bandwidth.exceptions.ApiException;
 import com.bandwidth.http.response.ApiResponse;
-import com.bandwidth.voice.models.RecordingMetadataResponse;
+import com.bandwidth.voice.models.*;
 
 import java.io.IOException;
 
@@ -11,17 +11,18 @@ public class Sample {
     public static final String ACCOUNT_ID = System.getenv("BW_ACCOUNT_ID");
 
     public static void main(String[] args) {
-        // Call id is returned after successfully creating a call.
         String callId = "c-95ac8d6e-1a31c52e-b38f-4198-93c1-51633ec68f8d";
-        // Recording id is returned after retrieving a recording from the call.
         String recordingId = "r-d68201ef-d53e-4c6d-a743-1c1283909d41";
 
         BandwidthClient client = new BandwidthClient.Builder()
                 .webRtcBasicAuthCredentials(USERNAME, PASSWORD)
                 .build();
 
+        ApiTranscribeRecordingRequest request = new ApiTranscribeRecordingRequest();
+        request.setCallbackUrl("https://sample.com/callbacks/transcribe");
+
         try {
-            ApiResponse<RecordingMetadataResponse> response = client.getVoiceClient().getAPIController().getMetadataForRecording(ACCOUNT_ID, callId, recordingId);
+            ApiResponse<Void> response = client.getVoiceClient().getAPIController().createTranscribeRecording(ACCOUNT_ID, callId, recordingId, request);
         } catch (ApiException|IOException ex) {
             // Handle exceptions from the request.
         }
