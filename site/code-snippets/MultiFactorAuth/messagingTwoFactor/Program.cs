41713e0b42b83df817cbdd1363b202b68fac0996
Mike Hamer <mhamer@bandwidth.com>
2021-07-27 11:09:20 -0400

    Update MFA to full snippets

---
+++ site/code-snippets/MultiFactorAuth/messagingTwoFactor/Program.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Bandwidth.Standard;
+using Bandwidth.Standard.TwoFactorAuth.Models;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var username = System.Environment.GetEnvironmentVariable("BW_USERNAME");
+        var password = System.Environment.GetEnvironmentVariable("BW_PASSWORD");
+        var accountId = System.Environment.GetEnvironmentVariable("BW_ACCOUNT_ID");
+        var applicationId = System.Environment.GetEnvironmentVariable("BW_MESSAGING_APPLICATION_ID");
+        var to = System.Environment.GetEnvironmentVariable("USER_NUMBER");
+        var from = System.Environment.GetEnvironmentVariable("BW_NUMBER");
+        var scope = "sample";
+        var digits = 6;
+        var message = "Your temporary {NAME} {SCOPE} code is {CODE}";
+
+        var client = new BandwidthClient.Builder()
+            .TwoFactorAuthBasicAuthCredentials(username, password)
+            .Build();
+
+        var request = new TwoFactorCodeRequestSchema
+        {
+            ApplicationId = applicationId,
+            To = to,
+            From = from,
+            Scope = scope,
+            Digits = digits,
+            Message = message
+        };
+
+        var response = await client.TwoFactorAuth.MFAController.CreateMessagingTwoFactorAsync(accountId, request);
+    }
+}
