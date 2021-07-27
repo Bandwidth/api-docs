1cced423c088ec430740103e85b46b76a3e32969
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-27 12:46:59 -0400

    Add hangup docs

--- site/sidebar.js
+++ site/sidebar.js
@@ -69,6 +69,7 @@ module.exports = {
             "voice/bxml/conference",
             "voice/bxml/forward",
             "voice/bxml/gather",
+            "voice/bxml/hangup",
           ],
         },
         {
