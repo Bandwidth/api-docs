afd1a9f6a06eb2107e0c605149877537723b5185
Mike Hamer <mhamer@bandwidth.com>
2021-07-27 11:03:51 -0400

    Void returned from delete recording media

--- site/code-snippets/Voice/deleteRecordingMedia/Program.cs
+++ site/code-snippets/Voice/deleteRecordingMedia/Program.cs
@@ -19,6 +19,6 @@ class Program
             .VoiceBasicAuthCredentials(username, password)
             .Build();
 
-        var response = await client.Voice.APIController.DeleteRecordingMediaAsync(accountId, callId, recordingId);
+        await client.Voice.APIController.DeleteRecordingMediaAsync(accountId, callId, recordingId);
     }
 }
