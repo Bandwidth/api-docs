7455e44f220c7f37157e69449881efb0c626132c
Mike Hamer <mhamer@bandwidth.com>
2021-07-27 13:31:21 -0400

    Add using for http client

--- site/code-snippets/Messaging/UploadMedia/Program.cs
+++ site/code-snippets/Messaging/UploadMedia/Program.cs
@@ -2,7 +2,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Bandwidth.Standard;
-using Bandwidth.Standard.Messaging.Models;
+using Bandwidth.Standard.Http.Client;
 
 class Program
 {
