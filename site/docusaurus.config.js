3ae949fdc1f418cb6d5726d791ddda660427d6b7
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-08-10 11:30:12 -0400

    throw on broken links

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -16,7 +16,7 @@ module.exports = {
   tagline: 'Learn About Bandwidth\'s Product API\'s',
   url: 'https://dev.bandwidth.com',
   baseUrl: '/',
-  onBrokenLinks: 'warn', //'throw',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'bandwidth', // Usually your GitHub org/user name.
