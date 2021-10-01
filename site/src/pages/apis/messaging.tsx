0521a0d9aeb3952d29c0015541216f788e677fd2
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-10-01 16:49:44 -0400

    Test

--- site/src/pages/apis/messaging.tsx
+++ site/src/pages/apis/messaging.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import ApiReference from '../../components/ApiReference';
+// import ApiReference from '../../components/ApiReference';
+
+import { API } from '@stoplight/elements';
+import '@stoplight/elements/styles.min.css';
 
 export default function ApiReferencePage() {
+
     const {siteConfig} = useDocusaurusContext();
     return (
-        <Layout
-          title={`Messaging API Reference`}
-          description="Authentication, Media, and Messages. Everything you'll need to start working with our Messaging API."
-          keywords="Bandwidth,API,Messaging,Media">
-            <ApiReference spec={siteConfig.customFields.messagingSpec} color={siteConfig.customFields.messagingGreen} />
+        <Layout>
+            <API apiDescriptionDocument={siteConfig.customFields.messagingSpec} />
         </Layout>
     );
 }
