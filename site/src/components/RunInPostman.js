3b2d4978761edf66a6a50ef8c3df1779cf75a832
Cameron Koegel <ckoegel@bandwidth.com>
2021-11-08 10:02:28 -0500

    Redid button to avoid confusion

--- site/src/components/RunInPostman.js
+++ site/src/components/RunInPostman.js
@@ -3,9 +3,6 @@ import styles from '../pages/index.module.css';
 
 export default function RunInPostman(props) {
     return (
-        <div className={styles.RunInPostman}>
-            <div className={styles.RunInPostmanLeft}/>
-            <a href="https://github.com/Bandwidth/postman" className={styles.RunInPostmanRight}>Run in Postman</a>
-        </div>
+        <a href="https://github.com/Bandwidth/postman" className={styles.RunInPostman}>Postman Collection</a>
     );
   }
