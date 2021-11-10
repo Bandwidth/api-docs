1fad9fcbef34c236495abffc0e7777eefdb272e6
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-11-10 12:30:59 -0500

    DX-2304 Add cURL samples for TN Lookup
    Added create and query samples
    
    updated `add-code-snippets-oas.js` to convert .sh files to curl
    
    started curl env replacement file

---
+++ site/code-snippets/PhoneNumberLookup/LookupRequestStatus/1-curl.sh
@@ -0,0 +1,2 @@
+curl --location --request GET 'https://numbers.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/tnlookup/004223a0-8b17-41b1-bf81-20732adf5590' \
+    --header 'Authorization: Basic $cURL_AUTH'
