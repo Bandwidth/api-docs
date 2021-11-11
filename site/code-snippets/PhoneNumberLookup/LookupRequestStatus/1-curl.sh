549658d781829676ca6a9cf74974a1b024c06ddf
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-11-11 10:26:37 -0500

    Update site/code-snippets/PhoneNumberLookup/LookupRequestStatus/1-curl.sh
    Co-authored-by: Mike Hamer <mhamer@bandwidth.com>

--- site/code-snippets/PhoneNumberLookup/LookupRequestStatus/1-curl.sh
+++ site/code-snippets/PhoneNumberLookup/LookupRequestStatus/1-curl.sh
@@ -1,2 +1,2 @@
-curl --location --request GET 'https://numbers.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/tnlookup/004223a0-8b17-41b1-bf81-20732adf5590' \
-    --header 'Authorization: Basic $cURL_AUTH'
+curl https://numbers.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/tnlookup/004223a0-8b17-41b1-bf81-20732adf5590 \
+    -H 'Authorization: Basic $cURL_AUTH'
