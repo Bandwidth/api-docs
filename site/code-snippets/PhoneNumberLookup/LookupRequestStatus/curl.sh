661ac39ff9f9e827a1c2dc97e7d615c5732a0970
Cameron Koegel <ckoegel@bandwidth.com>
2021-11-11 16:26:27 -0500

    removed old curl files

--- site/code-snippets/PhoneNumberLookup/LookupRequestStatus/curl.sh
+++
@@ -1,2 +0,0 @@
-curl --location --request GET 'https://numbers.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/tnlookup/004223a0-8b17-41b1-bf81-20732adf5590' \
-    --header 'Authorization: Basic $cURL_AUTH'
