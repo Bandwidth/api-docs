dde931c4ea27ccf74050c2eb010be78967378e99
Cameron Koegel <ckoegel@bandwidth.com>
2021-11-11 10:17:43 -0500

    added quotes to environment variables

--- site/code-snippets/MultiFactorAuth/messagingTwoFactor/1-curl.sh
+++ site/code-snippets/MultiFactorAuth/messagingTwoFactor/1-curl.sh
@@ -3,9 +3,9 @@ curl 'https://mfa.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/code/messaging' \
     -H 'Authorization: Basic $cURL_AUTH' \
     -H 'Content-Type: application/json' \
     -d '{
-        "to": $USER_NUMBER,
-        "from": $BW_MFA_NUMBER,
-        "applicationId": $BW_MFA_MESSAGING_APPLICATION_ID,
+        "to": "$USER_NUMBER",
+        "from": "$BW_MFA_NUMBER",
+        "applicationId": "$BW_MFA_MESSAGING_APPLICATION_ID",
         "scope": "scope",
         "message": "Your temporary {NAME} {SCOPE} code is {CODE}",
         "digits": 5
--- site/code-snippets/MultiFactorAuth/verifyTwoFactor/1-curl.sh
+++ site/code-snippets/MultiFactorAuth/verifyTwoFactor/1-curl.sh
@@ -3,8 +3,8 @@ curl 'https://mfa.bandwidth.com/api/v1/accounts/$BW_ACCOUNT_ID/code/verify' \
     -H 'Authorization: Basic $cURL_AUTH' \
     -H 'Content-Type: application/json' \
     -d '{
-        "to": $USER_NUMBER,
-        "applicationId": $BW_MFA_MESSAGING_APPLICATION_ID,
+        "to": "$USER_NUMBER",
+        "applicationId": "$BW_MFA_MESSAGING_APPLICATION_ID",
         "scope": "scope",
         "expirationTimeInMinutes": 3,
         "code": "123456"
