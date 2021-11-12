fecde8ba8f0178066b6ad60ab61e7694a163ae88
Cameron Koegel <ckoegel@bandwidth.com>
2021-11-12 14:15:57 -0500

    DX-2309 Created Ruby MFA Snippets

---
+++ site/code-snippets/MultiFactorAuth/messagingTwoFactor/sample.rb
@@ -0,0 +1,24 @@
+require 'bandwidth'
+
+include Bandwidth
+include Bandwidth::TwoFactorAuth
+
+bandwidth_client = Bandwidth::Client.new(
+    two_factor_auth_basic_auth_user_name: ENV['BW_USERNAME'],
+    two_factor_auth_basic_auth_password: ENV['BW_PASSWORD']
+)
+auth_client = bandwidth_client.two_factor_auth_client.mfa
+
+body = TwoFactorCodeRequestSchema.new
+body.application_id = ENV['BW_MESSAGING_APPLICATION_ID']
+body.to = [ENV['USER_NUMBER']]
+body.from = ENV['BW_NUMBER']
+body.digits = 6
+body.scope = "scope"
+body.message = "Your temporary {NAME} {SCOPE} code is {CODE}"
+begin
+    result = auth_client.create_messaging_two_factor(ENV['BW_ACCOUNT_ID'], body)
+    puts 'messageId: ' + result.data.message_id
+rescue APIException => e
+    puts e.response_code
+end
---
+++ site/code-snippets/MultiFactorAuth/verifyTwoFactor/sample.rb
@@ -0,0 +1,23 @@
+require 'bandwidth'
+
+include Bandwidth
+include Bandwidth::TwoFactorAuth
+
+bandwidth_client = Bandwidth::Client.new(
+    two_factor_auth_basic_auth_user_name: ENV['BW_USERNAME'],
+    two_factor_auth_basic_auth_password: ENV['BW_PASSWORD']
+)
+auth_client = bandwidth_client.two_factor_auth_client.mfa
+
+body = TwoFactorVerifyRequestSchema.new
+body.application_id = ENV['BW_VOICE_APPLICATION_ID']
+body.to = [ENV['USER_NUMBER']]
+body.scope = "scope"
+body.code = "123456"
+body.expiration_time_in_minutes = 3
+begin
+    result = auth_client.create_verify_two_factor(ENV['BW_ACCOUNT_ID'], body)
+    puts 'valid?: ' + result.data.valid
+rescue APIException => e
+    puts e.response_code
+end
