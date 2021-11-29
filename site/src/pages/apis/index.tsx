35a88c7ffd799aa42430030415c9b2ec1eaf9ca0
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-11-29 15:45:11 -0500

    Add empty API Reference Landing Page

---
+++ site/src/pages/apis/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from '../index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">Bandwidth APIs</h1>
+      <p className="hero__subtitle">Build with Bandwidth</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title="Bandwidth APIs"
+      description="Bandwidth API Reference, documentation, SDKs, guides, examples and more. Get everything you need to build with Bandwidth."
+      keywords="Bandwidth,API">
+      <HomepageHeader />
+    </Layout>
+  );
+}
