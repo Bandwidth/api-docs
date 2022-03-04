eb90a58b292df2bd73a363b034231c5614d9e105
Cameron Koegel <ckoegel@bandwidth.com>
2022-03-04 14:19:11 -0500

    remove code snippets

--- site/code-snippets/Messaging/GetMessages/Curl.sh
+++
@@ -1,2 +0,0 @@
-curl 'https://messaging.bandwidth.com/api/v2/users/$BW_ACCOUNT_ID/messages?sourceTn=$BW_NUMBER' \
-    -H 'Authorization: Basic $cURL_AUTH'
--- site/code-snippets/Voice/queryCallRecordings/Curl.sh
+++
@@ -1,2 +0,0 @@
-curl 'https://voice.bandwidth.com/api/v2/accounts/$BW_ACCOUNT_ID/recordings' \
-    -H 'Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ='
