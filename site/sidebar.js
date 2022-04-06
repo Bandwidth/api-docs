595844bbc26b4dde9afddb1a413b9b1555ac1d4f
mmateiuk <mmateiuk@cybervisiontech.com>
2022-04-06 10:38:22 +0300

    update /account | /address | /aeuis | /applications

--- site/sidebar.js
+++ site/sidebar.js
@@ -44,6 +44,7 @@ module.exports = {
                     type: "category",
                     label: "Guides and Tutorials",
                     items: [
+                        "numbers/applications",
                         "numbers/searchingNumbers",
                         "numbers/orderingNumbers",
                         "numbers/portingNumbers",
