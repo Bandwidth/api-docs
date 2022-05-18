536e5c1750d480b6624f5d3701959ed025ea6aba
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-05-18 16:21:07 -0400

    Fix colorMode syntax

--- site/src/components/ApiReference.js
+++ site/src/components/ApiReference.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import useThemeContext from '@theme/hooks/useThemeContext';    // returns whether or not the theme is light or dark
+import {useColorMode} from '@docusaurus/theme-common';
 import { RedocStandalone } from 'redoc';
 import { lightTheme, darkTheme } from '../css/redocTheme';
 
 const RedocConfig = (props) => {
-  const {isDarkTheme} = useThemeContext();
+  const {colorMode, setColorMode} = useColorMode();
   return <RedocStandalone spec={props.spec} options={{
                       scrollYOffset: '.navbar', // makes the fixed sidebar and scrolling play nicey with docusaurus navbar
                       pathInMiddlePanel: true,
                       expandSingleSchemaField: true,
                       expandResponses: "200,201,202,204",
                       menuToggle: true,
-                      theme: isDarkTheme ? darkTheme(props.color) : lightTheme(props.color)
+                      theme: colorMode === 'dark' ? darkTheme(props.color) : lightTheme(props.color)
                   }}/>
 }
 
