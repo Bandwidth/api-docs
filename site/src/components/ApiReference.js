cee7ae2dc193270360e48fd5bd7be0c49a336cb7
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-05-18 16:23:48 -0400

    Nice spacing

--- site/src/components/ApiReference.js
+++ site/src/components/ApiReference.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import {useColorMode} from '@docusaurus/theme-common';
+import { useColorMode } from '@docusaurus/theme-common';
 import { RedocStandalone } from 'redoc';
 import { lightTheme, darkTheme } from '../css/redocTheme';
 
