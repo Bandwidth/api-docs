4b07b679f370b626422d5987d46e5f5acae4eb66
Brian <bgome004@gmail.com>
2022-05-25 16:28:49 -0400

    DX-2623 Added Global Menu Item to Ext Docsite

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -60,6 +60,10 @@ module.exports = {
                 src: 'img/bandwidth-logo-navbar.png',
             },
             items: [{
+                label: 'US & Canada',
+                position: 'left',
+                className: 'regionLabel',
+            },{
                 to: 'docs',
                 activeBasePath: 'docs',
                 label: 'Docs',
@@ -76,6 +80,14 @@ module.exports = {
                 href: 'https://github.com/Bandwidth-Samples',
                 label: 'Samples',
                 position: 'left',
+            }, {
+                label: 'Global',
+                position: 'right',
+                className: 'regionLabel',
+            }, {
+                href: 'https://dev.bandwidth.com/apis/global/',
+                label: 'Docs & API Reference',
+                position: 'right',
             }]
         },
         footer: {
