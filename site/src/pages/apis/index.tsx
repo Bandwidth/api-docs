0dd3fb4c6a333b1398b1a1d166aeee490a822e89
Cameron Koegel <ckoegel@bandwidth.com>
2022-06-20 14:08:52 -0400

    DX-2631 Update Api Reference Page

--- site/src/pages/apis/index.tsx
+++ site/src/pages/apis/index.tsx
@@ -1,56 +1,103 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import apiStyles from '../../css/apis.module.css';
-import twoColumnStyles from '../../css/twoColumn.module.css';
-import ItemGrid from '../../components/ItemGrid';
 
-const apiList = [
-    {
-        title: 'Voice',
-        Svg: require('../../../static/img/icon-voice-complex-no-bg.svg').default,
-        link: '/apis/voice'
-    },
-    {
-        title: 'Messaging',
-        Svg: require('../../../static/img/icon-messaging-complex-no-bg.svg').default,
-        link: '/apis/messaging'
-    },
-    {
-        title: 'Numbers',
-        Svg: require('../../../static/img/icon-numbers-complex-no-bg.svg').default,
-        link: '/apis/numbers'
-    },
-    {
-        title: 'Phone Number Lookup',
-        Svg: require('../../../static/img/icon-phone-number-lookup-complex-no-bg.svg').default,
-        link: '/apis/number-lookup'
-    },
-    {
-        title: 'International Messaging',
-        Svg: require('../../../static/img/icon-int-messaging-complex-no-bg.svg').default,
-        link: '/apis/messaging-international'
-    },
-    {
-        title: 'Multi-Factor Authentication',
-        Svg: require('../../../static/img/icon-multi-factor-complex-no-bg.svg').default,
-        link: '/apis/multifactorauth'
-    },
-    {
-        title: 'WebRTC',
-        Svg: require('../../../static/img/icon-webrtc-complex-no-bg.svg').default,
-        link: '/apis/webrtc'
-    },
-    {
-        title: 'DASH',
-        Svg: require('../../../static/img/icon-dash-complex-no-bg.svg').default,
-        link: '/apis/dash'
-    },
-    {
-        title: 'DASH Notifications',
-        Svg: require('../../../static/img/icon-dash-notifications-complex-no-bg.svg').default,
-        link: '/apis/dash-notifications'
-    }
-];
+const apiList = {
+    numbers: [
+        {
+            name: 'Numbers',
+            link: '/apis/numbers'
+        },
+        {
+            name: 'Phone Number Lookup',
+            link: '/apis/number-lookup'
+        }
+    ],
+    voice: [
+        {
+            name: 'Voice',
+            link: '/apis/voice'
+        }
+    ],
+    messaging: [
+        {
+            name: 'Messaging',
+            link: '/apis/messaging'
+        },
+        {
+            name: 'International Messaging',
+            link: '/apis/messaging-international'
+        }
+    ],
+    emergency: [
+        {
+            name: 'DASH',
+            link: '/apis/dash'
+        },
+        {
+            name: 'DASH Notifications',
+            link: '/apis/dash-notifications'
+        }
+    ],
+    mfa: [
+        {
+            name: 'Multi-Factor Authentication',
+            link: '/apis/multifactorauth'
+        }
+    ],
+    webrtc: [
+        {
+            name: 'WebRTC',
+            link: '/apis/webrtc'
+        },
+    ]
+};
+
+function ProductInfo({productName, apis}) {
+    return (
+        <div className={apiStyles.productInfo}>
+            <h1>{productName} API</h1>
+            <div className={apiStyles.apiList}>
+                <ul>
+                    {apis.map((api, idx) => (
+                        <li key={idx}><a href={api.link}>{api.name}</a></li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function ProductGrid() {
+    return (
+        <div className={apiStyles.products}>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.numbersSvg}`}/>
+                <ProductInfo productName='Numbers' apis={apiList.numbers}/>
+            </div>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.voiceSvg}`}/>
+                <ProductInfo productName='Voice' apis={apiList.voice}/>
+            </div>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.messagingSvg}`}/>
+                <ProductInfo productName='Messaging' apis={apiList.messaging}/>
+            </div>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.emergencySvg}`}/>
+                <ProductInfo productName='Emergency' apis={apiList.emergency}/>
+            </div>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.mfaSvg}`}/>
+                <ProductInfo productName='Authentication' apis={apiList.mfa}/>
+            </div>
+            <div className={apiStyles.product}>
+                <svg className={`${apiStyles.productSvg} ${apiStyles.webrtcSvg}`}/>
+                <ProductInfo productName='WebRTC' apis={apiList.webrtc}/>
+            </div>
+        </div>
+    );
+}
 
 export default function Apis() {
     return (
@@ -58,16 +105,17 @@ export default function Apis() {
             title="Bandwidth APIs"
             description="Bandwidth API Reference, documentation, SDKs, guides, examples and more. Get everything you need to build with Bandwidth."
             keywords="Bandwidth,API">
-            <div className={twoColumnStyles.landing}>
-                <div className={twoColumnStyles.left}>
-                    <div className={twoColumnStyles.title}>
-                        <h1>Bandwidth API References</h1>
-                        <h2>Complete API Reference Documentation</h2>
+            <div className={apiStyles.landing}>
+                <div className={apiStyles.top}>
+                    <div className={apiStyles.title}>
+                        <h1>API Reference</h1>
+                        <h2>Technical reference for Bandwidth APIs</h2>
+                        <p>Our API Reference for US and Canada APIs is organized around product APIs and phone number management APIs.</p>
                     </div>
                     <div className={apiStyles.image}></div>
                 </div>
-                <div className={twoColumnStyles.right}>
-                    <ItemGrid itemList={apiList}/>
+                <div className={apiStyles.bottom}>
+                    <ProductGrid/>
                 </div>
             </div>
         </Layout>
