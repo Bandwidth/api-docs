ee78018ba8d799b97be5173edaf87bd0d672a719
Cameron Koegel <ckoegel@bandwidth.com>
2022-08-19 10:02:59 -0400

    change get started button to stay on same tab

--- site/src/components/DocsGetStarted.js
+++ site/src/components/DocsGetStarted.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import docsAboutStyles from '@site/src/components/css/DocsAbout.module.css';
-import BlueButton from '@site/src/components/BlueButton.js';
+import blueButtonStyles from '@site/src/components/css/BlueButton.module.css';
 
 export default function DocsGetStarted({getStartedText, Svg, getStartedButton}) {
     return (
         <div className={docsAboutStyles.getStarted}>
             <div className={docsAboutStyles.getStartedText}>{getStartedText}</div>
             <div className={docsAboutStyles.getStartedImage}><Svg/></div>
-            <BlueButton {...getStartedButton}/>
+            <div className={blueButtonStyles.blueButton}>
+                <a href={getStartedButton.link} rel="noopener">{getStartedButton.text}</a>
+            </div>
         </div>
     );
 }
