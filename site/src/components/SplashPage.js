4d89ee4c8c97090c804f155decf7bd9f43e6cdf8
Cameron Koegel <ckoegel@bandwidth.com>
2022-08-25 16:48:00 -0400

    prep for splash page changes

---
+++ site/src/components/SplashPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import splashPageStyles from '@site/src/components/css/SplashPage.module.css';
+
+export default function SplashPage() {
+    return (
+        <div className={splashPageStyles.splash}>Welcome To Bandwidth's Developer Docs</div>
+    );
+}
--- site/src/pages/docs/index.js
+++ site/src/pages/docs/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import LandingPage from '@site/src/components/LandingPage.js';
+import LandingPage from '@site/src/components/LandingPage';
 
-const docsList = [
+const productList = [
     {
         title: 'Account',
         Svg: require('@site/static/img/product-icons/account.svg').default,
@@ -11,49 +11,49 @@ const docsList = [
     {
         title: 'Numbers',
         Svg: require('@site/static/img/product-icons/numbers.svg').default,
-        link: '/docs/numbers'
-    },
-    {
-        title: 'Voice',
-        Svg: require('@site/static/img/product-icons/voice.svg').default,
-        link: '/docs/voice'
-    },
-    {
-        title: 'Messaging',
-        Svg: require('@site/static/img/product-icons/messaging.svg').default,
-        link: '/docs/messaging'
-    },
-    {
-        title: 'WebRTC',
-        Svg: require('@site/static/img/product-icons/webrtc.svg').default,
-        link: '/docs/webrtc'
-    },
-    {
-        title: 'Multi-Factor Authentication',
-        Svg: require('@site/static/img/product-icons/mfa.svg').default,
-        link: '/docs/mfa'
+        link: '/docs/numbers',
     },
     {
         title: 'Emergency',
         Svg: require('@site/static/img/product-icons/emergency.svg').default,
-        link: '/docs/emergency'
-    }
+        link: '/docs/emergency',
+    },
+    {
+        title: 'Voice',
+        Svg: require('@site/static/img/product-icons/voice.svg').default,
+        link: '/docs/voice',
+    },
+    {
+        title: 'Multi-Factor Authentication',
+        Svg: require('@site/static/img/product-icons/mfa.svg').default,
+        link: '/docs/mfa',
+    },
+    {
+        title: 'Messaging',
+        Svg: require('@site/static/img/product-icons/messaging.svg').default,
+        link: '/docs/messaging',
+    },
+    {
+        title: 'WebRTC',
+        Svg: require('@site/static/img/product-icons/webrtc.svg').default,
+        link: '/docs/webrtc',
+    },
 ];
 
 const props = {
-    itemList: docsList,
+    itemList: productList,
     Svg: require('@site/static/img/landing-home.svg').default,
     h1Text: `Bandwidth API Documentation`,
-    h2Text: `Guides for Bandwidth's Product APIs`
-}
+    h2Text: `Learn about Bandwidth's Product APIs`
+};
 
-export default function Docs() {
+export default function Home() {
     return (
         <Layout
-            title="Bandwidth Docs"
+            title={`Home`}
             description="Bandwidth API Reference, documentation, SDKs, guides, examples and more. Get everything you need to build with Bandwidth."
-            keywords="Bandwidth,Docs">
-            <LandingPage {...props}/>
+            keywords="Bandwidth,API,documentation,SDK,guide">
+            <LandingPage {...props} />
         </Layout>
     );
 }
