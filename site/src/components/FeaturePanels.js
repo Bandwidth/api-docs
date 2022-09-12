e885364da6a203b58be5795c7f852a46b98aff5b
Cameron Koegel <53310569+ckoegel@users.noreply.github.com>
2022-09-12 13:44:41 -0400

    DX-2816 Updates to Voice About Page (#706)
    * e911, edgemanagement, sipauthentication settings api spec (#701)
    
    Co-authored-by: ichesnov <ichesnov@cybervisiontech.com>
    
    * DX-2816 Updates to Voice About Page
    
    * workflow
    
    * branch
    
    * remove workflow change
    
    * Update hangup.mdx (#707)
    
    Fix a broken hyperlink
    
    * add stream panel
    
    * workflow
    
    * remove branch
    
    Co-authored-by: Ivan Chesnov <xxxlaykxxx@gmail.com>
    Co-authored-by: ichesnov <ichesnov@cybervisiontech.com>
    Co-authored-by: AJ Rice <53190766+ajrice6713@users.noreply.github.com>

--- site/src/components/FeaturePanels.js
+++ site/src/components/FeaturePanels.js
@@ -3,19 +3,15 @@ import featurePanelStyles from '@site/src/components/css/FeaturePanel.module.css
 
 function FeaturePanel({Svg, title, text, link}) {
     return (
-        <div className={featurePanelStyles.panel}>
+        <a className={featurePanelStyles.panel} href={link}>
             <div className={featurePanelStyles.header}>
                 <Svg className={featurePanelStyles.icon}/>
                 <div className={featurePanelStyles.title}>{title}</div>
             </div>
             <div className={featurePanelStyles.content}>
                 <div className={featurePanelStyles.panelText}>{text}</div>
-                <div className={featurePanelStyles.link}>
-                    <div className={featurePanelStyles.linkLabel}>{link.label}:</div>
-                    <div className={featurePanelStyles.linkText}><a href={link.to}>{link.text}</a></div>
-                </div>
             </div>
-        </div>
+        </a>
     );
 }
 
