747117613fc4cc4310c7ee64ca54a0985385f177
DX-Bandwidth <42817071+DX-Bandwidth@users.noreply.github.com>
2022-09-22 16:04:06 -0400

    DX-2480 (#435)
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Create Page for Porting API
    
    * Delete Zendesk comments in port order object - as this is not part of MVP
    
    * Test markdown table
    
    * Changed from JSON to YALM
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Convert json to yml
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Remove old JSON spec files
    
    * Delete porting.yml
    
    * Revert "Remove old JSON spec files"
    
    This reverts commit d63c521c1c1a0fe94eb16e8429b9b09eddcd1c7c.
    
    * Update site infra to support global v3 spec
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    * Fix porting page
    
    Keeping this for now so that it is still shown to customers
    
    * Add Code Snippets to Spec Files
    
    * Add Code Snippets to Spec Files
    
    Co-authored-by: AJ Rice <53190766+ajrice6713@users.noreply.github.com>
    Co-authored-by: cphan-band <92993189+cphan-band@users.noreply.github.com>
    Co-authored-by: matthewkmartin <matthewmartin@bandwidth.com>

---
+++ site/src/pages/apis/porting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import ApiReference from '../../components/ApiReference';
+
+export default function ApiReferencePage() {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+          title={`Porting API Reference`}
+          description="Everything you'll need to start working with our Porting API."
+          keywords="Bandwidth,API,Porting">
+            <ApiReference spec={siteConfig.customFields.globalSpec_v3} color={siteConfig.customFields.numbersMaroon} />
+        </Layout>
+    );
+}
