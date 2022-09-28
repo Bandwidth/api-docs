ca503a51a0e91fdb6607bccc74e2e7dd544f50c6
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-28 09:01:01 -0400

    Update insights.js

--- site/src/pages/apis/insights.js
+++ site/src/pages/apis/insights.js
@@ -7,7 +7,7 @@ export default function ApiReferencePage() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-          title={`Phone Insights API Reference`}
+          title={`Insights API Reference`}
           description="Account metrics and information. Everything you'll need to start working with our Insights API."
           keywords="Bandwidth,API,Insights,Metrics">
             <ApiReference spec={siteConfig.customFields.insightsSpec} color={siteConfig.customFields.voicePurple} downloadDefinitionUrl="https://github.com/Bandwidth/api-docs/blob/main/site/specs/insights.yml"/>
