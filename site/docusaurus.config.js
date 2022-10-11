bc2405ca6332ef0a9dc2c34ed3d7bfab7c18492f
Cameron Koegel <ckoegel@bandwidth.com>
2022-10-11 11:45:51 -0400

    DX-2799 Use New Numbers Spec

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -2,7 +2,7 @@ const YAML = require('yaml');
 const fs = require('fs');
 const path = require('path');
 
-const numbersSpec = fs.readFileSync('./specs-temp/numbers.json', 'utf-8');
+const numbersSpec = fs.readFileSync('./specs/numbers.yml', 'utf-8');
 const phoneNumberLookupSpec = fs.readFileSync('./specs/phone-number-lookup.yml', 'utf-8');
 const voiceSpec = fs.readFileSync('./specs/voice.yml', 'utf-8');
 const messagingSpec = fs.readFileSync('./specs/messaging.yml', 'utf-8');
