a621b39de7b7103d07b3516c88cf01bb6d8d4546
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-10-14 15:11:33 -0400

    Add Migration Guide Sidebar Test

---
+++ site/cypress/e2e/tests/migration_guides_sidebar.cy.js
@@ -0,0 +1,15 @@
+
+import {testSidebar} from '../../utils/utils'
+
+context('Testing the Migration Guides Sidebar Tabs', () => {
+  beforeEach(() => {
+    cy.visit('/migration-guides')
+  })
+
+  it('Should verify that the Python tab and subtabs opens and closes properly', () => {
+    testSidebar('Python')
+    testSidebar('About')
+    testSidebar('v14 -> v15')
+  })
+
+})
