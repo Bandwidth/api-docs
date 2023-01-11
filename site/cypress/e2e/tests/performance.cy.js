f732f2781bfcd7cbbdb5bfe1eb58aec5665bf86f
Brian <bgome004@gmail.com>
2023-01-11 09:06:51 -0500

    Update performance.cy.js

--- site/cypress/e2e/tests/performance.cy.js
+++ site/cypress/e2e/tests/performance.cy.js
@@ -4,17 +4,17 @@ context('Home Page Performance Test', () => {
   performanceTester('/', 'Bandwidth Developer Documentation')
 })
 
-context('Home Page Performance Test', () => {
+context('Docs Page Performance Test', () => {
   performanceTester('/docs/', 'Bandwidth API Documentation')
 })
 
-context('Home Page Performance Test', () => {
+context('API Reference Page Performance Test', () => {
   performanceTester('/apis/', 'API Reference')
 })
 
-context('Home Page Performance Test', () => {
-  performanceTester('/apis/numbers/', 'Response samples', 'h3')
+context('Numbers API Performance Test', () => {
+  performanceTester('/apis/numbers/', 'Response samples', 'h3', 10000)
 })
-context('Home Page Performance Test', () => {
-  performanceTester('/apis/global/', 'Access', 'h2')
+context('Global API Performance Test', () => {
+  performanceTester('/apis/global/', 'Access', 'h2', 10000)
 })
