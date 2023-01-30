0756055f0d59a4f7d451a19f21226ca336026ab2
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2023-01-30 15:37:30 -0500

    Apply Prettier Formatting

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -6,14 +6,26 @@ const customConfig = require('./custom.config.json');
 const blogPosts = require('./blogposts.config.json');
 const ltsVersions = require('./lts.config.json');
 const numbersSpec = fs.readFileSync('./specs/numbers.yml', 'utf-8');
-const phoneNumberLookupSpec = fs.readFileSync('./specs/phone-number-lookup.yml', 'utf-8');
+const phoneNumberLookupSpec = fs.readFileSync(
+  './specs/phone-number-lookup.yml',
+  'utf-8'
+);
 const voiceSpec = fs.readFileSync('./specs/voice.yml', 'utf-8');
 const messagingSpec = fs.readFileSync('./specs/messaging.yml', 'utf-8');
 const webRtcSpec = fs.readFileSync('./specs/webrtc.yml', 'utf-8');
-const multiFactorAuthSpec = fs.readFileSync('./specs/multi-factor-auth.yml', 'utf-8');
+const multiFactorAuthSpec = fs.readFileSync(
+  './specs/multi-factor-auth.yml',
+  'utf-8'
+);
 const dashSpec = fs.readFileSync('./specs/dash.json', 'utf-8');
-const dashNotificationsSpec = fs.readFileSync('./specs/dashNotifications.json', 'utf-8');
-const messagingInternationalSpec = fs.readFileSync('./specs/messagingInternational.json', 'utf-8');
+const dashNotificationsSpec = fs.readFileSync(
+  './specs/dashNotifications.json',
+  'utf-8'
+);
+const messagingInternationalSpec = fs.readFileSync(
+  './specs/messagingInternational.json',
+  'utf-8'
+);
 const globalSpec = fs.readFileSync('./specs/global.yml', 'utf-8');
 const globalSpec_v2 = fs.readFileSync('./specs/global-v2.yml', 'utf-8');
 const globalSpec_v3 = fs.readFileSync('./specs/global-v3.yml', 'utf-8');
@@ -24,169 +36,178 @@ const identitySpec = fs.readFileSync('./specs/one-identity-management.yml', 'utf
 */
 
 module.exports = {
-    title: 'Bandwidth API Docs',
-    tagline: 'Learn About Bandwidth\'s Product APIs',
-    url: 'https://dev.bandwidth.com',
-    baseUrl: '/',
-    onBrokenLinks: 'warn',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    trailingSlash: undefined,
-    organizationName: 'bandwidth',
-    projectName: 'api-docs',
-    themeConfig: {
-        image: 'img/bw-icon.svg', // used for meta tag
-        colorMode: {
-            disableSwitch: false // for disabling dark mode
-        },
-        algolia: {
-            appId: 'DJ56W2DX1L',
-            apiKey: '865921f3caf63083e8d54552b5ab7a79',
-            indexName: 'bandwidth',
-            contextualSearch: false, // useful for versioned Docusaurus sites
-        },
-        navbar: {
-            title: '',
-            hideOnScroll: false,
-            logo: {
-                alt: 'Bandwidth',
-                src: 'img/dev-docs-logo.svg',
-            },
-            items: [{
-                type: 'dropdown',
-                label: 'US & Canada APIs',
-                position: 'left',
-                items: [
-                    {
-                        to: 'docs',
-                        activeBasePath: 'docs',
-                        label: 'Guides',
-                    }, {
-                        to: 'apis',
-                        label: 'API Reference',
-                        activeBasePath: 'apis'
-                    }, {
-                        to: 'sdks',
-                        label: 'SDKs',
-                        activeBasePath: 'sdks'
-                    }, {
-                        to: 'https://github.com/Bandwidth-Samples',
-                        label: 'Samples',
-                    }
-                ]
-            }, {
-                type: 'dropdown',
-                label: 'Global APIs',
-                position: 'left',
-                items: [
-                    {
-                        to: 'apis/global',
-                        label: 'Docs & API Reference',
-                    }
-                ]
-            }, {
-                href: 'https://github.com/Bandwidth',
-                position: 'right',
-                className: 'github-link',
-                title: 'Github Organization'
-            }, {
-                href: 'https://www.postman.com/bandwidth',   // TODO: update with real postman url
-                position: 'right',
-                className: 'postman-link',
-                title: 'Postman Collection'
-            }, {
-                href: 'https://www.bandwidth.com/login/',
-                position: 'right',
-                className: 'login-link',
-                title: 'Login'
-            }]
-        },
-        // Now this breaks redoc :sad: (Now missing prism dependency as well)
-        // prism: {
-        //     additionalLanguages: ['csharp', 'java', 'ruby', 'php'],
-        //   },
+  title: 'Bandwidth API Docs',
+  tagline: "Learn About Bandwidth's Product APIs",
+  url: 'https://dev.bandwidth.com',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  trailingSlash: undefined,
+  organizationName: 'bandwidth',
+  projectName: 'api-docs',
+  themeConfig: {
+    image: 'img/bw-icon.svg', // used for meta tag
+    colorMode: {
+      disableSwitch: false, // for disabling dark mode
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            {
-                docs: {
-                    sidebarPath: require.resolve('./sidebar.js'),
-                    editUrl: 'https://github.com/Bandwidth/api-docs/edit/main/site/',
-                    sidebarCollapsible: true,
-                },
-                blog: {
-                    showReadingTime: true,
-                    editUrl: 'https://github.com/Bandwidth/api-docs/edit/main/site/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/main.scss'),
-                },
-                googleAnalytics: {
-                    trackingID: 'UA-62651840-1',
-                    anonymizeIP: false,
-                },
-            }
-        ],
-    ],
-    customFields: {
-        numbersSpec: YAML.parse(numbersSpec),
-        numbersSpecLink: `${customConfig.numbersSpecLink}`,
-        phoneNumberLookupSpec: YAML.parse(phoneNumberLookupSpec),
-        phoneNumberLookupSpecLink: `${customConfig.phoneNumberLookupSpecLink}`,
-        voiceSpec: YAML.parse(voiceSpec),
-        voiceSpecLink: `${customConfig.voiceSpecLink}`,
-        messagingSpec: YAML.parse(messagingSpec),
-        messagingSpecLink: `${customConfig.messagingSpecLink}`,
-        messagingInternationalSpec: JSON.parse(messagingInternationalSpec),
-        messagingInternationalSpecLink: `${customConfig.messagingInternationalSpecLink}`,
-        webRTCSpec: YAML.parse(webRtcSpec),
-        webRtcSpecLink: `${customConfig.webRtcSpecLink}`,
-        multiFactorAuthSpec: YAML.parse(multiFactorAuthSpec),
-        multiFactorAuthSpecLink: `${customConfig.multiFactorAuthSpecLink}`,
-        dashSpec: JSON.parse(dashSpec),
-        dashSpecLink: `${customConfig.dashSpecLink}`,
-        dashNotificationsSpec: JSON.parse(dashNotificationsSpec),
-        dashNotificationsSpecLink: `${customConfig.dashNotificationsSpecLink}`,
-        globalSpec: YAML.parse(globalSpec),
-        globalSpec_v2: YAML.parse(globalSpec_v2),
-        globalSpec_v3: YAML.parse(globalSpec_v3),
-        globalSpec_beta: YAML.parse(globalSpec_beta),
-        insightsSpec: YAML.parse(insightsSpec),
-        insightsSpecLink: `${customConfig.insightsSpecLink}`,
-        // TODO ONEID-1304
-        // identitySpec: YAML.parse(identitySpec),
-
-        blogPosts: blogPosts,
-
-        ltsVersions: ltsVersions,
-
-        // CSS Colors
-        bwBlue: '#079CEE',
-        voicePurple: '#9a59c5',
-        messagingGreen: '#00bf8c',
-        emergencyOrange: '#ff6f47',
-        numbersMaroon: '#652B51',
-        webRtcBlue: '#194F7A',
-        mfaGreen: '#106149',
-        white: '#FFFFFF',
-        lightBlue: '#E6F5FD',
-        grey: '#9C9A9B',
-        midnight: '#084f7A',
-        black: '#090306',
-        redocCodeBackground: '#263238',
+    algolia: {
+      appId: 'DJ56W2DX1L',
+      apiKey: '865921f3caf63083e8d54552b5ab7a79',
+      indexName: 'bandwidth',
+      contextualSearch: false, // useful for versioned Docusaurus sites
     },
-    plugins: [
-        path.resolve(__dirname, 'redoc-plugin'),
-        [
-            '@docusaurus/plugin-content-docs',
+    navbar: {
+      title: '',
+      hideOnScroll: false,
+      logo: {
+        alt: 'Bandwidth',
+        src: 'img/dev-docs-logo.svg',
+      },
+      items: [
+        {
+          type: 'dropdown',
+          label: 'US & Canada APIs',
+          position: 'left',
+          items: [
             {
-                id: 'miration-guides',
-                path: 'migration-guides',
-                routeBasePath: 'migration-guides',
-                sidebarPath: require.resolve('./sidebarsMigrationGuides.js'),
+              to: 'docs',
+              activeBasePath: 'docs',
+              label: 'Guides',
             },
-        ],
-        'docusaurus-plugin-sass',
+            {
+              to: 'apis',
+              label: 'API Reference',
+              activeBasePath: 'apis',
+            },
+            {
+              to: 'sdks',
+              label: 'SDKs',
+              activeBasePath: 'sdks',
+            },
+            {
+              to: 'https://github.com/Bandwidth-Samples',
+              label: 'Samples',
+            },
+          ],
+        },
+        {
+          type: 'dropdown',
+          label: 'Global APIs',
+          position: 'left',
+          items: [
+            {
+              to: 'apis/global',
+              label: 'Docs & API Reference',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/Bandwidth',
+          position: 'right',
+          className: 'github-link',
+          title: 'Github Organization',
+        },
+        {
+          href: 'https://www.postman.com/bandwidth', // TODO: update with real postman url
+          position: 'right',
+          className: 'postman-link',
+          title: 'Postman Collection',
+        },
+        {
+          href: 'https://www.bandwidth.com/login/',
+          position: 'right',
+          className: 'login-link',
+          title: 'Login',
+        },
+      ],
+    },
+    // Now this breaks redoc :sad: (Now missing prism dependency as well)
+    // prism: {
+    //     additionalLanguages: ['csharp', 'java', 'ruby', 'php'],
+    //   },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebar.js'),
+          editUrl: 'https://github.com/Bandwidth/api-docs/edit/main/site/',
+          sidebarCollapsible: true,
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/Bandwidth/api-docs/edit/main/site/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/main.scss'),
+        },
+        googleAnalytics: {
+          trackingID: 'UA-62651840-1',
+          anonymizeIP: false,
+        },
+      },
     ],
+  ],
+  customFields: {
+    numbersSpec: YAML.parse(numbersSpec),
+    numbersSpecLink: `${customConfig.numbersSpecLink}`,
+    phoneNumberLookupSpec: YAML.parse(phoneNumberLookupSpec),
+    phoneNumberLookupSpecLink: `${customConfig.phoneNumberLookupSpecLink}`,
+    voiceSpec: YAML.parse(voiceSpec),
+    voiceSpecLink: `${customConfig.voiceSpecLink}`,
+    messagingSpec: YAML.parse(messagingSpec),
+    messagingSpecLink: `${customConfig.messagingSpecLink}`,
+    messagingInternationalSpec: JSON.parse(messagingInternationalSpec),
+    messagingInternationalSpecLink: `${customConfig.messagingInternationalSpecLink}`,
+    webRTCSpec: YAML.parse(webRtcSpec),
+    webRtcSpecLink: `${customConfig.webRtcSpecLink}`,
+    multiFactorAuthSpec: YAML.parse(multiFactorAuthSpec),
+    multiFactorAuthSpecLink: `${customConfig.multiFactorAuthSpecLink}`,
+    dashSpec: JSON.parse(dashSpec),
+    dashSpecLink: `${customConfig.dashSpecLink}`,
+    dashNotificationsSpec: JSON.parse(dashNotificationsSpec),
+    dashNotificationsSpecLink: `${customConfig.dashNotificationsSpecLink}`,
+    globalSpec: YAML.parse(globalSpec),
+    globalSpec_v2: YAML.parse(globalSpec_v2),
+    globalSpec_v3: YAML.parse(globalSpec_v3),
+    globalSpec_beta: YAML.parse(globalSpec_beta),
+    insightsSpec: YAML.parse(insightsSpec),
+    insightsSpecLink: `${customConfig.insightsSpecLink}`,
+    // TODO ONEID-1304
+    // identitySpec: YAML.parse(identitySpec),
+
+    blogPosts: blogPosts,
+
+    ltsVersions: ltsVersions,
+
+    // CSS Colors
+    bwBlue: '#079CEE',
+    voicePurple: '#9a59c5',
+    messagingGreen: '#00bf8c',
+    emergencyOrange: '#ff6f47',
+    numbersMaroon: '#652B51',
+    webRtcBlue: '#194F7A',
+    mfaGreen: '#106149',
+    white: '#FFFFFF',
+    lightBlue: '#E6F5FD',
+    grey: '#9C9A9B',
+    midnight: '#084f7A',
+    black: '#090306',
+    redocCodeBackground: '#263238',
+  },
+  plugins: [
+    path.resolve(__dirname, 'redoc-plugin'),
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'miration-guides',
+        path: 'migration-guides',
+        routeBasePath: 'migration-guides',
+        sidebarPath: require.resolve('./sidebarsMigrationGuides.js'),
+      },
+    ],
+    'docusaurus-plugin-sass',
+  ],
 };
