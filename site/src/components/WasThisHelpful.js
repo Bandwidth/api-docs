122ad11919640558a8a0d39fc2fabeab0d2a1cd8
ckoegel <ckoegel1006@gmail.com>
2023-05-12 15:43:21 -0400

    give container a height

--- site/src/components/WasThisHelpful.js
+++ site/src/components/WasThisHelpful.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function WasThisHelpful() {
     return (
-        <div className="was-this-helpful" id="was-this-helpful"/>
+        <div className="was-this-helpful" id="was-this-helpful" style={{height: '100px'}}/>
     );
 }
