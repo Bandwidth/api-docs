4821437aff5f3011c6f1aca30e7525ea8f028acd
ckoegel <ckoegel1006@gmail.com>
2023-05-25 16:41:16 -0400

    test faster site build

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -226,4 +226,14 @@ module.exports = {
     ],
     "docusaurus-plugin-sass",
   ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
 };
