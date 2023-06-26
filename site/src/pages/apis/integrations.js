f6ecc794e070063b6167b408126caa50b79d6c9b
Mostafa Habib <mhabib@bandwidth.com>
2023-06-26 12:07:08 -0400

    BS-1132 - flatten integrations and use config options

--- site/src/pages/apis/integrations.js
+++ site/src/pages/apis/integrations.js
@@ -8,8 +8,8 @@ export default function ApiReferencePage() {
     return (
         <Layout
             title={`Integrations API Reference`}
-            description="Account integrations and xcaas. Everything you'll need to start working with our Integrations API."
-            keywords="Bandwidth,API,Integrations,Xcaas">
+            description="Account integrations. Everything you'll need to start working with our Integrations API."
+            keywords="Bandwidth,API,Integrations">
             <ApiReference spec={siteConfig.customFields.integrationsSpec} color={siteConfig.customFields.voicePurple} downloadDefinitionUrl={siteConfig.customFields.integrationsSpecLink}/>
         </Layout>
     );
