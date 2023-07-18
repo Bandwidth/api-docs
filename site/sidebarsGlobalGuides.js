a6293905ccb2bb3c83d16d63cdcc10bebe75eff0
Cameron Koegel <53310569+ckoegel@users.noreply.github.com>
2023-07-18 10:44:17 -0400

    SWI-2948 Add Section For Global Guides (#1062)
    * SWI-2948 Add Section For Global Guides
    
    * update cypress tests
    
    ---------
    
    Co-authored-by: AJ Rice <53190766+ajrice6713@users.noreply.github.com>

---
+++ site/sidebarsGlobalGuides.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mySidebar: [
+    {
+      type: "doc",
+      id: "global-guides",
+      label: "Introduction",
+    },
+    {
+      type: "category",
+      label: "Category",
+      items: [
+        "category/category-specific-guide",
+      ]
+    }
+  ],
+};
