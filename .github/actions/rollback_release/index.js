b2075dde63f04f9cd8112d644ba2f1273abff3c8
ckoegel <ckoegel1006@gmail.com>
2023-08-11 10:16:05 -0400

    this is JS not ruby :face_with_spiral_eyes:

--- .github/actions/rollback_release/index.js
+++ .github/actions/rollback_release/index.js
@@ -31,14 +31,14 @@ async function rollbackRelease() {
   core.setOutput("goodRelease", goodRelease)
 
   workflows = await octokit.request('GET /repos/Bandwidth/api-docs/actions/workflows')
-  publish_id = workflows.data.workflows.find(x => x.name === 'Publish Docsite').id
+  publishId = workflows.data.workflows.find(x => x.name === 'Publish Docsite').id
   
   await octokit.request(`PATCH /repos/Bandwidth/api-docs/releases/${badReleaseId}`, {
     tag_name: badRelease,
     draft: true,
   })
   
-  await octokit.request(`POST /repos/Bandwidth/api-docs/actions/workflows/${publish_id}/dispatches`, {
+  await octokit.request(`POST /repos/Bandwidth/api-docs/actions/workflows/${publishId}/dispatches`, {
     ref: goodRelease,
   })
 }
